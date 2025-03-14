13d45ab6bcd5987462486e82a428826e2b8efac4
Stanislav <stanislav3@gmail.com>
2025-03-14 22:15:04 +0300

    update step 5

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 from brain_games import cli
+from random import randint
 
+
+def quiz(user_name):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        number = randint(1, 100)
+        print(f'Question: {number}')
+        answer = input('Your answer: ')
+        while not answer in ['yes', 'no']:
+            answer = input('Your answer: ')
+        if not number % 2 and answer == "yes" or number % 2 and answer == "no":
+            print('Correct!')
+        else:
+            print(f"'{answer}'  is wrong answer ;(. Correct answer was '{'no' if answer == 'yes' else 'yes'}'.")
+            print(f"Let's try again, {user_name}!")
+    print(f"Congratulations, {user_name}!")
 
 def main():
     cli.print_hello()
     name = cli.enter_name()
     print(cli.welcome_user(name))
+    quiz(name)
 
 
 if __name__ == '__main__':
